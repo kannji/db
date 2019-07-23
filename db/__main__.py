313a6823df731c621149e7514d208c448feb9c62
Jan-Luca Klees <email@janlucaklees.de>
2019-07-23 14:36:01 +0200

    Reworked writings insert
    In this commit I reworked the way how writings are inserted into the
    database. I did this in preparation to also add readings in the next few
    commits. What I did here is develop a technique that would allow me to
    insert writings and readings in a similar fashion, with little code
    duplication and without having troubles with existing content in the
    database.

--- db/__main__.py
+++ db/__main__.py
@@ -1,4 +1,5 @@
 import uuid
+import logging
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,12 +9,17 @@ from lxml import etree
 from .Base import Base, dbURL, Node, Edge, EdgeTypes
 
 
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 db_engine = create_engine(dbURL)
 
 Base.metadata.create_all(db_engine)
 
 SessionFactory = sessionmaker(bind=db_engine)
-session = SessionFactory()
+
 
 def getReadingFromEntry(entry):
     return entry.xpath("r_ele/reb/text()")[0]
@@ -26,41 +32,71 @@ def getWritingFromEntry(entry):
     else:
         return getReadingFromEntry(entry)
 
-def addWritings():
-
-    # Get a set of writings from the JMdict
-    jmdict_writings = set()
+def getWritingSetFromJMdict():
+    writings = set()
     for _event, entry in etree.iterparse("./db/JMdict_e.xml", tag="entry"):
         writing = getWritingFromEntry(entry)
-        jmdict_writings.add(writing)
+        writings.add(writing)
 
-    print(len(jmdict_writings))
+    return writings
 
-    # Get a set of writings from the database
-    result = session \
-            .query(Node) \
+def getContentSet(node_set):
+    return set([n.content for n in node_set])
+
+def getMatchingNodesQuery(session, match_set):
+    return session.query(Node).filter(Node.content.in_(match_set))
+
+def getMatchingNodes(session, match_set):
+    return getMatchingNodesQuery(session, match_set).all()
+
+def getMatchingNodesOfType(session, match_set, type):
+    return getMatchingNodesQuery(session, match_set) \
             .join(Node.incoming_edges) \
-            .filter(Edge.type == EdgeTypes.writing) \
+            .filter(Edge.type == type) \
             .all()
-    db_writings = set([writing.content for writing in result])
 
-    print(len(db_writings))
+def addNodes(session, content_set):
+    session.bulk_save_objects([Node(content=content) for content in content_set])
 
-    # Get the difference, to know which writings to add
-    new_writings = jmdict_writings - db_writings
+def addReflexiveEdges(session, node_set, type):
+    session.bulk_save_objects([Edge(source_uuid=n.uuid, target_uuid=n.uuid, type=type) for n in node_set])
 
-    print(len(new_writings))
+def addWritingNodesForWritingSet(jmdict_writings):
+    session = SessionFactory()
 
-    # Add the writings, and mark them as such with an Edge
-    if len(new_writings) > 0:
-        insert = []
-        for writing in new_writings:
-            new_node = Node(uuid=uuid.uuid4(), content=writing)
-            new_edge = Edge(source_uuid=new_node.uuid, target_uuid=new_node.uuid, type=EdgeTypes.writing)
-            insert.append(new_node)
-            insert.append(new_edge)
-        session.bulk_save_objects(insert)
-        session.commit()
+    existing_writings = getContentSet(getMatchingNodes(session, jmdict_writings))
+    logger.info(str(len(existing_writings)) + " nodes with matching content already existing in database.")
+
+    missing_writings = jmdict_writings - existing_writings
+
+    if missing_writings:
+        addNodes(session, missing_writings)
+
+    session.commit()
+    logger.info(str(len(missing_writings)) + " nodes added to the database.")
+
+def addWritingEdgesForWritingSet(jmdict_writings):
+    session = SessionFactory()
+
+    typed_writings = getContentSet(getMatchingNodesOfType(session, jmdict_writings, EdgeTypes.writing))
+    logger.info(str(len(typed_writings)) +
+                " writings with Edge of type `writing` found in the database.")
+
+    untyped_writings = jmdict_writings - typed_writings
+
+    if untyped_writings:
+        untyped_nodes = getMatchingNodes(session, untyped_writings)
+        addReflexiveEdges(session, untyped_nodes, EdgeTypes.writing)
+
+    session.commit()
+    logger.info(str(len(untyped_writings)) + " edges added to the database.")
+
+
+def addWritings():
+    jmdict_writings = getWritingSetFromJMdict()
+    logger.info(str(len(jmdict_writings)) + " writings found in JMdict.")
+
+    addWritingNodesForWritingSet(jmdict_writings)
+    addWritingEdgesForWritingSet(jmdict_writings)
 
 addWritings()
-
